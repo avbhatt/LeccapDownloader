dce71b2b196997dd339bb97d32a40acd1d1184f4
mnajork <mnajork@umich.edu>
2016-04-21 23:21:28 -0400

    Initial implementation

---
+++ README.md
@@ -0,0 +1,26 @@
+# leccap_dl
+
+An automated lecture recording downloader designed to work with https://leccap.engin.umich.edu.
+
+This implementation downloads all lectures for a given course into the current working directory.
+
+Requires Google Chrome and Selenium (`$ pip install selenium`).
+
+## Setup
+
+1. Download the most recent version ChromeDriver for your operating system from https://sites.google.com/a/chromium.org/chromedriver/downloads
+2. Extract the ChromeDriver zip and move the binary into the same directory as leccap_dl.py
+3. Set the `UNIQNAME` and `PASSWORD` fields in leccap_dl.py
+
+## Usage
+
+`python leccap_dl.py course_uid file_prefix`
+
+**Name** | **Type** | **Description**
+--- | --- | ---
+`course_uid` | string | **Required.** The unique course identifier, which can be found at the end of the leccap URL. Note that this is not the same as the unique identifier for an individual lecture recording.
+`file_prefix` | string | **Required.** The filename prefix. Lecture recordings are saved as the prefix followed by an integer.
+
+#### Example
+
+`python leccap_dl.py hsfrlzcioe7xc71tu1w eecs482_lecture`
---
+++ leccap_dl.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+import argparse
+import urllib
+from selenium import webdriver
+
+UNIQNAME = # CHANGEME
+PASSWORD = # CHANGEME
+
+FILE_EXT = ".mp4"
+
+LOGIN_URL = "https://weblogin.umich.edu/"
+LECCAP_BASE_URL = "https://leccap.engin.umich.edu/leccap/viewer/s/"
+
+def parse_args():
+    parser = argparse.ArgumentParser(\
+        description="An automated leccap recording downloader",\
+        epilog="example: python leccap_dl.py hsfrlzcioe7xc71tu1w eecs482_lecture")
+    parser.add_argument("course_uid",\
+        help="the unique leccap course identifier")
+    parser.add_argument("file_prefix",\
+        help="the file prefix for downloaded lecture recordings")
+
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+
+    # initialize browser
+    browser = webdriver.Chrome(executable_path = './chromedriver')
+    browser.implicitly_wait(60) # seconds
+
+    # attempt login
+    browser.get(LOGIN_URL)
+    browser.find_element_by_id("login").send_keys(UNIQNAME)
+    browser.find_element_by_id("password").send_keys(PASSWORD)
+    browser.find_element_by_id("loginSubmit").click()
+
+    # go to course leccap page
+    leccap_course_url = LECCAP_BASE_URL + args.course_uid
+    browser.get(leccap_course_url)
+
+    # scrape lecture urls
+    lecture_urls = []
+    for rec_btn in browser.find_elements_by_class_name("recording-button"):
+        lec_url = rec_btn.get_attribute("href")
+        lecture_urls.append(lec_url)
+
+    # scrape video urls from lectures
+    video_urls = []
+    for lec_url in lecture_urls:
+        browser.get(lec_url)
+
+        vid_url = browser.find_element_by_tag_name("video").get_attribute("src")
+        video_urls.append(vid_url)
+
+    browser.quit()
+
+    # download videos
+    for i in range(len(video_urls)):
+        filename = args.file_prefix + str(i + 1) + FILE_EXT
+
+        print("downloading " + filename + " from " + video_urls[i])
+        urllib.urlretrieve(video_urls[i], filename)
+
+if __name__ == '__main__':
+    main()
