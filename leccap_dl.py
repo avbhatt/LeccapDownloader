72261073ddaca0f7dc66e5985d105b84a07716d2
Adit Bhatt <avbhatt@umich.edu>
2017-03-20 14:32:46 -0400

    feat: add multithreaded feature, download number of lectures, name, output directory

--- leccap_dl.py
+++ leccap_dl.py
@@ -1,9 +1,11 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 
 import argparse
-import urllib
+import requests
+from clint.textui import progress
 import getpass
 from selenium import webdriver
+import threading
 
 FILE_EXT = ".mp4"
 
@@ -11,58 +13,93 @@ LOGIN_URL = "https://weblogin.umich.edu/"
 LECCAP_BASE_URL = "https://leccap.engin.umich.edu/leccap/viewer/s/"
 
 def parse_args():
-    parser = argparse.ArgumentParser(\
-        description="An automated leccap recording downloader",\
-        epilog="example: python leccap_dl.py hsfrlzcioe7xc71tu1w eecs482_lecture")
-    parser.add_argument("course_uid",\
-        help="the unique leccap course identifier")
-    parser.add_argument("file_prefix",\
-        help="the file prefix for downloaded lecture recordings")
+	parser = argparse.ArgumentParser(\
+		description="An automated leccap recording downloader",\
+		epilog="example: python leccap_dl.py hsfrlzcioe7xc71tu1w [-o /home/user/videos] [-t]")
+	parser.add_argument("course_uid",\
+		help="the unique leccap course identifier")
+	parser.add_argument("-o", "--output-directory",\
+		default='.',\
+		help="directory to output files (default: current directory [.])")
+	parser.add_argument("-t", "--threaded",help="if used, each download will be put in a new thread",action="store_true")
 
-    return parser.parse_args()
+	return parser.parse_args()
 
 def main():
-    args = parse_args()
+	args = parse_args()
 
-    uniqname = raw_input("Uniqname: ")
-    password = getpass.getpass("Password: ")
+	uniqname = input("Uniqname: ")
+	password = getpass.getpass("Password: ")
 
-    # initialize browser
-    browser = webdriver.Chrome(executable_path = './chromedriver')
-    browser.implicitly_wait(60) # seconds
+	# initialize browser
+	browser = webdriver.Chrome('./chromedriver')
+	browser.implicitly_wait(60) # seconds
 
-    # attempt login
-    browser.get(LOGIN_URL)
-    browser.find_element_by_id("login").send_keys(uniqname)
-    browser.find_element_by_id("password").send_keys(password)
-    browser.find_element_by_id("loginSubmit").click()
+	# attempt login
+	browser.get(LOGIN_URL)
+	browser.find_element_by_id("login").send_keys(uniqname)
+	browser.find_element_by_id("password").send_keys(password)
+	browser.find_element_by_id("loginSubmit").click()
 
-    # go to course leccap page
-    leccap_course_url = LECCAP_BASE_URL + args.course_uid
-    browser.get(leccap_course_url)
+	# go to course leccap page
+	leccap_course_url = LECCAP_BASE_URL + args.course_uid
+	browser.get(leccap_course_url)
 
-    # scrape lecture urls
-    lecture_urls = []
-    for rec_btn in browser.find_elements_by_class_name("recording-button"):
-        lec_url = rec_btn.get_attribute("href")
-        lecture_urls.append(lec_url)
+	# scrape lecture urls
+	lecture_urls = []
+	lecture_names = []
+	i = 0
+	for rec_btn in browser.find_elements_by_class_name("recording-button"):
+		lec_url = rec_btn.get_attribute("href")
+		lecture_urls.append(lec_url)
+		for rec_info in rec_btn.find_elements_by_class_name("recording-info"):
+			date = rec_info.find_element_by_class_name("recording-date").text
+			name = rec_info.find_element_by_class_name("recording-title").text
+			lecture_names.append(name)
+			print("[%d] Name: %s \t Date: %s" % (i,name, date))
+		i += 1
+	true_urls = []
+	print("Select video(s) to download (space delimited)")
+	select_indices = [int(x) for x in input().split()]
+	true_urls = [(lecture_urls[i], lecture_names[i]) for i in select_indices]
+	video_urls = []
+	for (lec_url, useless) in true_urls:
+		browser.get(lec_url)
+		vid_url = browser.find_element_by_tag_name("video").get_attribute("src")
+		video_urls.append(vid_url)
 
-    # scrape video urls from lectures
-    video_urls = []
-    for lec_url in lecture_urls:
-        browser.get(lec_url)
+	browser.quit()
 
-        vid_url = browser.find_element_by_tag_name("video").get_attribute("src")
-        video_urls.append(vid_url)
+	output_directory = args.output_directory
+	if output_directory[-1] == '/':
+		output_directory = output_directory[:-1]
 
-    browser.quit()
+	# download videos
+	threads = []
+	for i in range(len(video_urls)):
+		filename =  output_directory + '/' + true_urls[i][1] + FILE_EXT
+		if args.threaded:
+			threads.append(threading.Thread(target=download_file, args=(filename, video_urls[i])))
+		else:
+			print("downloading " + filename + " from " + video_urls[i])
+			r = requests.get(video_urls[i], stream=True)
+			with open(filename, 'wb') as f:
+				total_length = int(r.headers.get('content-length'))
+				for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1):
+					if chunk:
+						f.write(chunk)
+	if args.threaded:
+		for i in threads:
+			i.start()
 
-    # download videos
-    for i in range(len(video_urls)):
-        filename = args.file_prefix + str(i + 1) + FILE_EXT
-
-        print("downloading " + filename + " from " + video_urls[i])
-        urllib.urlretrieve(video_urls[i], filename)
+def download_file(filename, url):
+	print("downloading " + filename + " from " + url)
+	r = requests.get(url, stream=True)
+	f = open(filename, 'wb')
+	for chunk in r.iter_content(chunk_size=1024):
+		if chunk:
+			f.write(chunk)
+	f.close()
 
 if __name__ == '__main__':
-    main()
+	main()
